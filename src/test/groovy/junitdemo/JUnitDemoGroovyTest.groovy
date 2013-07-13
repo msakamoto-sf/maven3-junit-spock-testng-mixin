6ac29dd8733ff28785f1f1cbd60285250a5f20e2
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2013-07-13 20:01:32 +0900

    - copy sample test methods from msakamoto-sf/gradle-junit-spock-testng-mixin.

--- src/test/groovy/junitdemo/JUnitDemoGroovyTest.groovy
+++ src/test/groovy/junitdemo/JUnitDemoGroovyTest.groovy
@@ -29,4 +29,33 @@ class JUnitDemoGroovyTest
         def actual = 1 + 2
         assertEquals(expected, actual)
     }
+
+    @Test
+    void JUnitWithGroovyPowerAssert()
+    {
+        def s1 = [1, 2, 3, 3, 4, 1, 3, 2] as Set
+        def s2 = [4, 2, 1, 3] as Set
+        def s3 = new HashSet([1, 3, 2, 4, 4])
+        assert s1 == s2
+        assert s2 == s3
+        assert s3 == s1
+
+        def l1 = [100, 200, 300]
+        def l2 = [100, 200, 300]
+        assert l1 == l2
+
+        def m1 = [
+            key1: 100,
+            key2: [100, 200, 300],
+            key3: [k1: 100, k2: 200, k3: 300],
+            key4: null,
+        ]
+        def m2 = [
+            key1: 100,
+            key2: [100, 200, 300],
+            key3: [k1: 100, k2: 200, k3: 300],
+            key4: null,
+        ]
+        assert m1 == m2
+    }
 }
--- src/test/groovy/testngdemo/TestNGDemoGroovyTest.groovy
+++ src/test/groovy/testngdemo/TestNGDemoGroovyTest.groovy
@@ -80,6 +80,35 @@ class TestNGDemoGroovyTest
         assertEquals(list.size(), 3)
     }
 
+    @Test
+    void TestNGWithGroovyPowerAssert()
+    {
+        def s1 = [1, 2, 3, 3, 4, 1, 3, 2] as Set
+        def s2 = [4, 2, 1, 3] as Set
+        def s3 = new HashSet([1, 3, 2, 4, 4])
+        assert s1 == s2
+        assert s2 == s3
+        assert s3 == s1
+
+        def l1 = [100, 200, 300]
+        def l2 = [100, 200, 300]
+        assert l1 == l2
+
+        def m1 = [
+            key1: 100,
+            key2: [100, 200, 300],
+            key3: [k1: 100, k2: 200, k3: 300],
+            key4: null,
+        ]
+        def m2 = [
+            key1: 100,
+            key2: [100, 200, 300],
+            key3: [k1: 100, k2: 200, k3: 300],
+            key4: null,
+        ]
+        assert m1 == m2
+    }
+
     @DataProvider(name="dp1")
     Object[][] dp1() {
         return [
@@ -104,12 +133,12 @@ class TestNGDemoGroovyTest
             [1, 2, 3],
             [4, 5, 9],
             [6, 7, 13]
-            ]
+        ]
     }
 
     @Test(dataProvider = "dp2")
     void verifyAddFunc2(int a, int b, int expected) {
-        assertEquals(testadd(a, b), expected);
+        assert testadd(a, b) == expected;
     }
 
     @DataProvider(name="dp3")
@@ -119,11 +148,11 @@ class TestNGDemoGroovyTest
             ["a", "", 1],
             ["", "a", 1],
             ["a", "b", 2],
-            ]
+        ]
     }
 
     @Test(dataProvider = "dp3")
     void dataproviderSample3(String a, String b, int expected) {
-        assertEquals((a + b).length(), expected)
+        assert (a + b).length() == expected
     }
 }
